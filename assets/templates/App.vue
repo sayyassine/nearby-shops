f605e2155eaafd6040c3b313d9af8c44824bf34e
Yassine Say <sayyassine@gmail.com>
2020-01-03 17:40:20 +0100

    Webpack Encore installed and configured & Vue installed

---
+++ assets/templates/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{message}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data : function () {
+            return {
+                message: "Hello world."
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
